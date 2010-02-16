bad86dff8d6a06e35092d18644338ba7b96f6a35
James Squires <jamessqr@gmail.com>
2010-02-15 18:40:09 -0600

    Wired up the data save for the entries

--- blog/admin/views.py
+++ blog/admin/views.py
@@ -6,5 +6,10 @@ from django.shortcuts import render_to_response
 def index(request):
 	if request.method == 'GET':
 		entryform = admin_forms.EntryForm()
+	if request.method == 'POST':
+		entryform = admin_forms.EntryForm(request.POST)
+		if entryform.is_valid():
+			entry = entryform.save()
+			#return render_to_response('index.html')		
 	payload = dict(entryform=entryform)
 	return render_to_response('index.html',payload)
