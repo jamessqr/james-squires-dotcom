ea1ed6f62a3c17e6e1cd3e36633c58501c5a8a0f
James <jamessqr@gmail.com>
2012-04-28 20:05:39 -0500

    Starting on admin tool

--- blog/admin/models.py
+++ blog/admin/models.py
@@ -1,12 +1,13 @@
 from google.appengine.ext import db
 from django import newforms as forms
+import datetime
 
 class Entry(db.Model):
 	title = db.StringProperty()
-	description = db.StringProperty()
+	description = db.TextProperty()
 	slug = db.StringProperty() #double check datatype
-	created_on = db.DateTimeProperty()
-	created_by = db.UserProperty()
+	created_on = db.DateTimeProperty(auto_now="true")
+	created_by = db.UserProperty(auto_current_user="true")
 	
 	#TODO
 	#Tags, categories
