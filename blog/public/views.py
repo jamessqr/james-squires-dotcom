32f113744c9f6480f461c0fac46ebec66b04a163
James <jamessqr@gmail.com>
2012-04-30 11:15:15 -0500

    fixed up permalinks to properly throw a 404 when it doesntexist

--- blog/public/views.py
+++ blog/public/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from blog.admin.models import Entry
+from django.http import Http404
 from django.shortcuts import get_object_or_404, render_to_response
 from google.appengine.ext import db
 
@@ -15,7 +16,11 @@ def about(request):
 	return render_to_response('about.html')
 	
 def entry_detail(request, queryset, slug):
+	#TODO: Figure out a way to get get_object_or_404 to work
 	Entries = Entry.all().filter('slug', slug)
 	#Entries = db.GqlQuery("SELECT * FROM Entry WHERE slug = :slug", slug=slug)
 	
+	if (Entries.count() < 1):
+		raise Http404()
+		
 	return render_to_response('entry_detail.html',{ 'Entries': Entries})
--- blog/settings.py
+++ blog/settings.py
@@ -1,7 +1,7 @@
 import os
 # Django settings for blog_app project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
