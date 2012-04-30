c5b97515d5f5416ca5e33056f2cb96f29c86e74d
James <jamessqr@gmail.com>
2012-04-29 23:27:32 -0500

    created entry detail page and wired up generic view for it

--- blog/public/views.py
+++ blog/public/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from blog.admin import models
-from django.shortcuts import render_to_response
+from blog.admin.models import Entry
+from django.shortcuts import get_object_or_404, render_to_response
 from google.appengine.ext import db
 
 def index(request):
@@ -13,3 +13,9 @@ def archive(request):
 	
 def about(request):
 	return render_to_response('about.html')
+	
+def entry_detail(request, queryset, slug):
+	Entries = Entry.all().filter('slug', slug)
+	#Entries = db.GqlQuery("SELECT * FROM Entry WHERE slug = :slug", slug=slug)
+	
+	return render_to_response('entry_detail.html',{ 'Entries': Entries})
--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls.defaults import *
+from google.appengine.ext import db
+from blog.admin.models import Entry
+#TODO: Import the Entry model for the url pattern
+
+entry_info_dict = {
+	'queryset': Entry.all(),
+}
 
 urlpatterns = patterns('',
 	(r'^admin/$', 'blog.admin.views.index'),
 	(r'^$', 'blog.public.views.index'),
 	(r'^archive/', 'blog.public.views.archive'),
 	(r'^about/', 'blog.public.views.about'),
+	(r'^(?P<slug>[a-zA-Z0-9_.-]+)/$', 'blog.public.views.entry_detail', entry_info_dict),
     # Example:
     # (r'^blog_app/', include('blog_app.foo.urls')),
 
