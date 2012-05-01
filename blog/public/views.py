6a027c74e2678ea88cd010ab795ed5f5c94b3888
James <jamessqr@gmail.com>
2012-04-30 21:05:52 -0500

    adding atom feed skeleton view, added meta description

--- blog/public/views.py
+++ blog/public/views.py
@@ -24,3 +24,6 @@ def entry_detail(request, queryset, slug):
 		raise Http404()
 		
 	return render_to_response('entry_detail.html',{ 'Entries': Entries})
+	
+def feed(request):
+	return render_to_response('feed.html')
