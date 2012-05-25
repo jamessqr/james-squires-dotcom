a7f6c93a677f15da6ccd26bc976174d881cab1af
James <jamessqr@gmail.com>
2012-05-24 23:06:10 -0500

    fixed a bug with the pubDate not showing in the syndication feed

--- blog/feeds.py
+++ blog/feeds.py
@@ -1,28 +1,26 @@
 from django.contrib.syndication.views import Feed
 from blog.models import Entry
 from django.utils.feedgenerator import Rss201rev2Feed
+import datetime
 
 class CorrectMimeTypeFeed(Rss201rev2Feed):
-    mime_type = 'application/xml'
+	mime_type = 'application/xml'
 
 # This is a django.contrib.syndication.feeds.Feed subclass whose feed_type
 # is set to our preferred MIME type.
 class BlogFeed(Feed):
-    feed_type = CorrectMimeTypeFeed
-    	
-class LatestEntriesFeed(BlogFeed):	
-    title = "James Squires - An outdoor journal"
-    link = "http://www.james-squires.com"
-    description = "James Squires is an outdoor enthusiast with passions for bowhunting midwest whitetails and fly fishing the small streams of the driftless area."
+	feed_type = CorrectMimeTypeFeed
 
-    def items(self):
-        return Entry.objects.order_by('-pub_date')[:10]
+class LatestEntriesFeed(BlogFeed):
+	title = "James Squires - An outdoor journal"
+	link = "http://www.james-squires.com"
+	description = "James Squires is an outdoor enthusiast with passions for bowhunting midwest whitetails and fly fishing the small streams of the driftless area."
 
-	def item_title(self, item):
-		return item.title
-		
+	def items(self):
+		return Entry.objects.order_by('-pub_date')[:10]
+
 	def item_pubdate(self, item):
 		return item.pub_date
 
-    def item_description(self, item):
+	def item_description(self, item):
 		return item.body
