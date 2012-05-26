81a657c875c69134fb9d395aee747a8ef669c75d
James <jamessqr@gmail.com>
2012-05-25 22:17:59 -0500

    cleaning up external feeds and fixed a bug with homepage story ordering

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render_to_response
 from blog.models import Entry
 
 def entries_index(request):
-	return render_to_response('blog/entry_index.html', { 'entry_list': Entry.objects.all() })
+	return render_to_response('blog/entry_index.html', { 'entry_list': Entry.objects.order_by('-pub_date')[:10] })
 	
 def archive(request):
 	return render_to_response('blog/entry_archive.html')
